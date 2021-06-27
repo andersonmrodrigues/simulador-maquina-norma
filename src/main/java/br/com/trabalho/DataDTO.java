dbbb103d80fa50c62631426d9add83cc227945e5
Anderson Rodrigues <maikbru@gmail.com>
2021-06-26 21:02:03 -0300

    iterative done

--- src/main/java/br/com/trabalho/DataDTO.java
+++ src/main/java/br/com/trabalho/DataDTO.java
@@ -18,9 +18,12 @@ public class DataDTO {
 
     public static final String SEPARATOR = "------------------------------------";
     private BigInteger registers;
-    private File file;
+    private File fileMonolithic;
+    private File fileIterative;
     private LinkedHashMap<String, BigInteger> registersMap;
-    private LinkedHashMap<Integer, String> fileLines;
+    private LinkedHashMap<Integer, String> fileMonolithicLines;
+    private LinkedHashMap<Integer, String> fileIterativeLines;
+    private Integer iterativeLastLineNumber;
     private Logger logger;
 
     public void createRegistersMap() {
@@ -33,15 +36,40 @@ public class DataDTO {
         logger.info("Registradores Gerados");
     }
 
-    public void createFileMap() {
+    public void iterativeMap() {
         int count = 1;
-        fileLines = new LinkedHashMap<>();
+        fileIterativeLines = new LinkedHashMap<>();
         logger.info("Lendo linhas do arquivo");
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileIterative))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                fileIterativeLines.put(count, line
+                        .trim().replaceAll(";", "")
+                        .replaceAll("\\)", "")
+                        .replaceAll("\\(", "")
+                );
+                count++;
+            }
+        } catch (FileNotFoundException e) {
+            logger.warning("Arquivo não encontrado");
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.warning("Erro ao ler arquivo");
+            e.printStackTrace();
+        }
+        this.iterativeLastLineNumber = count;
+        logger.info("Total de " + count + " linhas lidas");
+    }
+
+    public void monolithicMap() {
+        int count = 1;
+        fileMonolithicLines = new LinkedHashMap<>();
+        logger.info("Lendo linhas do arquivo");
+        try (BufferedReader br = new BufferedReader(new FileReader(fileMonolithic))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] value = line.split(":");
-                fileLines.put(Integer.valueOf(value[0]), value[1].trim());
+                fileMonolithicLines.put(Integer.valueOf(value[0]), value[1].trim());
                 count++;
             }
         } catch (FileNotFoundException e) {
@@ -52,7 +80,26 @@ public class DataDTO {
             e.printStackTrace();
         }
         logger.info("Total de " + count + " linhas lidas");
+    }
 
+    public boolean isMatchesEnquantoFaca(String line) {
+        return line.matches("(.*)enquanto ([a-zA-Z]*)_zero faça(.*)");
+    }
+
+    public boolean isMatchesSeSenao(String line) {
+        return line.matches("(.*)se ([a-zA-Z]*)_zero então (.+) senão (.+)(.*)");
+    }
+
+    public boolean isMatchesAd(String line) {
+        return line.matches("(.*)ad_([a-zA-Z]*)(.*)");
+    }
+
+    public boolean isMatchesSub(String line) {
+        return line.matches("(.*)sub_([a-zA-Z]*)(.*)");
+    }
+
+    public boolean isMatchesAteFaca(String line) {
+        return line.matches("(.*)até ([a-zA-Z]*)_zero faça(.*)");
     }
 
 }
--- src/main/java/br/com/trabalho/MachineService.java
+++ src/main/java/br/com/trabalho/MachineService.java
@@ -14,7 +14,8 @@ import java.util.logging.SimpleFormatter;
 public class MachineService {
 
     private DataDTO data;
-    private static final String FILEPATH = "D:\\Projetos\\simulador-maquina-norma\\exemplo_mono.txt";
+    private static final String PATHMONOLITHIC = "D:\\Projetos\\simulador-maquina-norma\\monolitico.txt";
+    private static final String PATHITERATIVE = "D:\\Projetos\\simulador-maquina-norma\\iterativo.txt";
     private static final String FILELOGPATH = "D:\\Projetos\\simulador-maquina-norma\\log.txt";
 
 
@@ -24,13 +25,14 @@ public class MachineService {
     }
 
     void execute() throws IOException {
-        setup();
+        setLogParams();
         int choice;
         do {
 
             System.out.println(DataDTO.SEPARATOR);
             System.out.println("Opções de Execução");
             System.out.println("1 - Ler programa monolítico do arquivo TXT");
+            System.out.println("2 - Converter programa iterativo para monolítico");
             System.out.println("8 - Ver Registradores");
             System.out.println("9 - Reiniciar tudo (ler arquivo, inicializar registradores, etc.)");
             System.out.println("0 - Encerrar Execução");
@@ -38,27 +40,109 @@ public class MachineService {
             choice = new Scanner(System.in).nextInt();
             switch (choice) {
                 case 1:
+                    setupMonolithic();
                     monolithicProgram();
+                    break;
+                case 2:
+                    setupIterative();
+                    convertIterative();
                     break;
                 case 8:
                     showRegisters();
                     break;
                 case 9:
-                    setup();
+                    setupMonolithic();
                     break;
             }
         } while (choice != 0);
     }
 
-    private void setup() throws IOException {
+    private void setLogParams() throws IOException {
         FileHandler fh = new FileHandler(FILELOGPATH);
         log.addHandler(fh);
         fh.setFormatter(new SimpleFormatter());
         log.setUseParentHandlers(false);
-        log.info("Verificando se existe o arquivo na caminho especificado");
-        data.setFile(checkIfFileExists());
-        log.info("Arquivo encontrado");
-        data.createFileMap();
+    }
+
+    private void convertIterative() {
+        StringBuilder monolithic = new StringBuilder();
+        Set<Integer> keys = data.getFileIterativeLines().keySet();
+        int instruction = 0;
+        for (Integer key : keys) {
+            String line = data.getFileIterativeLines().get(key);
+            instruction = setInstruction(monolithic, instruction);
+            String[] commands = line.split(" ");
+            if (data.isMatchesAteFaca(line) || data.isMatchesEnquantoFaca(line)) {
+                monolithic
+                        .append("se ")
+                        .append(commands[1])
+                        .append(" então vá_para 999 senão vá_para ")
+                        .append(instruction + 1)
+                        .append(System.lineSeparator());
+            } else if (!data.isMatchesSeSenao(line) && (data.isMatchesAd(line) || data.isMatchesSub(line))) {
+                monolithic
+                        .append("faça ")
+                        .append(commands[0])
+                        .append(" vá_para ")
+                        .append(instruction >= getLast() ? 1 : instruction + 1)
+                        .append(System.lineSeparator());
+            } else if (data.isMatchesSeSenao(line)) {
+                monolithic
+                        .append("se ")
+                        .append(commands[1])
+                        .append(" então vá_para ")
+                        .append(instruction + 1)
+                        .append(" senão vá_para ")
+                        .append(instruction + 2)
+                        .append(System.lineSeparator());
+                instruction = setInstruction(monolithic, instruction);
+                monolithic.append("faça ")
+                        .append(commands[3])
+                        .append(" vá_para ")
+                        .append(instruction >= getLast() ? 1 : instruction + 1)
+                        .append(System.lineSeparator());
+                instruction = setInstruction(monolithic, instruction);
+                monolithic.append("faça ")
+                        .append(commands[5])
+                        .append(" vá_para ")
+                        .append(instruction >= getLast() ? 1 : instruction + 1)
+                        .append(System.lineSeparator());
+            }
+        }
+        printMonolithic(monolithic);
+    }
+
+    private int getLast() {
+        return data.getIterativeLastLineNumber() - 1;
+    }
+
+    private void printMonolithic(StringBuilder monolithic) {
+        System.out.println(DataDTO.SEPARATOR);
+        System.out.println("Resultado da Conversão:");
+        System.out.println(monolithic.toString());
+        System.out.println(DataDTO.SEPARATOR);
+    }
+
+
+    private int setInstruction(StringBuilder monolithic, int instruction) {
+        instruction++;
+        monolithic.append(instruction);
+        monolithic.append(": ");
+        return instruction;
+    }
+
+    private void setupIterative() {
+        log.info("Verificando se existe o arquivo programa iterativo no caminho especificado");
+        data.setFileIterative(checkIfFileExists(PATHITERATIVE));
+        log.info("Arquivo programa iterativo encontrado");
+        data.iterativeMap();
+    }
+
+    private void setupMonolithic() {
+        log.info("Verificando se existe o arquivo programa monolitico no caminho especificado");
+        data.setFileMonolithic(checkIfFileExists(PATHMONOLITHIC));
+        log.info("Arquivo programa monolitico encontrado");
+        data.monolithicMap();
         data.setRegisters(getIntWithQuestion("Informe a quantidade de Registradores"));
         data.createRegistersMap();
         initializeRegisters();
@@ -66,13 +150,8 @@ public class MachineService {
     }
 
     private void monolithicProgram() {
-        //   0       1        2           3       4     5         6      7
-//        1: se     zero_b   então      vá_para   9   senão    vá_para   2
-//        2: faça   ad_a     vá_para    3
-//        3: faça   ad_a     vá_para    4
-//        4: faça   sub_b    vá_para    1
         log.info("Lendo primeira instrução");
-        executeLine(data.getFileLines().get(1));
+        executeLine(data.getFileMonolithicLines().get(1));
     }
 
     private void executeLine(String line) {
@@ -120,7 +199,7 @@ public class MachineService {
     }
 
     private void verifyIfExistsAndCallExecute(String command) {
-        String line = data.getFileLines().getOrDefault(Integer.valueOf(command), null);
+        String line = data.getFileMonolithicLines().getOrDefault(Integer.valueOf(command), null);
         log.info("Indo para a instrução " + command);
         if (line != null) {
             executeLine(line);
@@ -131,12 +210,14 @@ public class MachineService {
     }
 
     private boolean verifyRegister(String command) {
-        String register = command.replace("zero_", "");
+        String register = command.replaceAll("_zero", "");
         log.info("registrador '" + register + "' é igual a ZERO");
         return data.getRegistersMap().get(register).equals(BigInteger.ZERO) || data.getRegistersMap().get(register).equals(new BigInteger("0"));
     }
 
     private void showRegisters() {
+        if (data == null || data.getRegistersMap() == null || data.getRegistersMap().isEmpty())
+            return;
         Set<String> keys = data.getRegistersMap().keySet();
         log.info("------------Registradores-----------");
         for (String key : keys) {
@@ -161,8 +242,8 @@ public class MachineService {
         return new Scanner(System.in).nextBigInteger();
     }
 
-    public File checkIfFileExists() {
-        File file = new File(FILEPATH);
+    public File checkIfFileExists(String path) {
+        File file = new File(path);
         if (!file.exists()) {
             log.warning("Arquivo não encontrado");
             System.exit(1);
